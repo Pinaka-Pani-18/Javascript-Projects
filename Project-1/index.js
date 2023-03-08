a85abf2bfe28e05eff8eccfc1a1450c87380f907
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-03-08 23:30:21 +0530

    used another method

--- Project-1/index.js
+++ Project-1/index.js
@@ -1,14 +1,17 @@
 const btnEl = document.querySelector(".btn");
 const bodyEl = document.querySelector("body");
 
-var colors = ["green","blue","red","orange","yellow","volet"];
+var colors = ["green","blue","red","orange","yellow","violet"];
 
 bodyEl.style.background = "pink";
 
 btnEl.addEventListener("click",addColor);
 
+let count=0;
+
 function addColor(){
     console.log("clicked");
-    var color = parseInt(Math.random()*colors.length);
-    bodyEl.style.background = colors[color];
+    // var color = parseInt(Math.random()*colors.length);
+    bodyEl.style.background = colors[count%colors.length];
+    count++;
 }
