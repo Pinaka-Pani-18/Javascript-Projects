c8adf9498f43b01145993ca40dac5680aeaf2542
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-03-20 23:18:45 +0530

    added and removed fade class in js

--- Project-7/index.js
+++ Project-7/index.js
@@ -6,10 +6,12 @@ var URL = "https://v2.jokeapi.dev/joke/Any?type=single&amount=1";
 btnEl.addEventListener("click",getMethod);
 
 async function getMethod(){
+    jokeEl.classList.remove("fade");
         const data = await fetch(URL).then((e)=>
             e.json());
         if(data){
             console.log(data);
             jokeEl.innerHTML = data.joke;
+            jokeEl.classList.add("fade");
         }
     }
