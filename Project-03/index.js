32e6aac0302af12432d1ce326f8a1450231a6cda
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-04-02 18:06:26 +0530

    added reset button

--- Project-03/index.js
+++ Project-03/index.js
@@ -1,5 +1,6 @@
 const prevEl = document.querySelector(".dec")
 const nextEl = document.querySelector(".inc")
+const resetEl = document.querySelector(".reset");
 const buttons = document.querySelector(".btn")
 let countEl = document.querySelector(".count");
 
@@ -7,6 +8,7 @@ let counts = 0;
 
 prevEl.addEventListener("click",decrease);
 nextEl.addEventListener("click",increase);
+resetEl.addEventListener("click",reset);
 
 function increase(){
     counts++;
@@ -28,4 +30,8 @@ function decrease(){
         countEl.style.color = "black";
     }
 }
-
+function reset(){
+    counts=0;
+    countEl.innerHTML = counts;
+    countEl.style.color = "black";
+}
