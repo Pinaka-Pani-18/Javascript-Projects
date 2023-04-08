e85627a722eb2dc94a59d0c295d0f40d5305b738
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-04-08 16:41:37 +0530

    js code is complated

---
+++ Project-10/index.js
@@ -0,0 +1,32 @@
+const imageContainerEl = document.querySelector(".image-container");
+const prevEl = document.querySelector(".prev");
+const nextEl = document.querySelector(".next");
+
+let x=0;
+let timer=0;
+
+prevEl.addEventListener("click",()=>{
+    x = x + 45;
+    clearTimeout(timer);
+    updateContainer();
+    console.log("prev")
+})
+
+nextEl.addEventListener("click",()=>{
+    x = x - 45;
+    clearTimeout(timer);
+    updateContainer();
+    console.log("next")
+})
+
+function updateContainer(){
+    imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+
+    timer = setTimeout(() => {
+        x = x - 45;
+        updateContainer();
+    console.log("upd")
+    }, 2000);
+}
+
+updateContainer();
