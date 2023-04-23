2d88e1ee54609b9194179f5a72170ce6c6f27cee
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-04-23 12:38:12 +0530

    js code is completed

---
+++ Project-22/index.js
@@ -0,0 +1,11 @@
+// Check out below repository for more information about this Typed.js 
+// (Github repo-->Readme.md-->Customization)
+
+// https://github.com/mattboldt/typed.js/
+
+var typed = new Typed(".text-type",{
+    strings: ["Web Developer","Programmer","Open Source Contributor","Youtuber"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop : true
+})
