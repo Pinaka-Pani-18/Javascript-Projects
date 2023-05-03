df49904e16547306e831a1e4d4489cffa794e73f
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-05-03 17:52:38 +0530

    js code is completed

---
+++ Project-31/index.js
@@ -0,0 +1,17 @@
+const titleEl = document.querySelector(".title");
+const memeEl = document.querySelector(".meme");
+const authorEl = document.querySelector(".author");
+const generateMemeBtnEl = document.querySelector(".generateMemeBtn");
+
+async function generateMeme(){
+    const response = await fetch("https://meme-api.com/gimme/wholesomememes");
+    const data = await response.json()
+    titleEl.innerHTML = data.title;
+    memeEl.setAttribute("src",data.url);
+    authorEl.innerHTML = `Meme Created By ${data.author}`
+    console.log(data);
+}
+
+generateMemeBtnEl.addEventListener("click",generateMeme)
+
+generateMeme();
