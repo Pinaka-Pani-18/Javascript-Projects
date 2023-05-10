80f5540f26bd5e846e18370ec4d53b800e796318
Pinaka-Pani-18 <pinakapani1806@gmail.com>
2023-05-10 22:28:31 +0530

    changed colors

--- Project-06/index.js
+++ Project-06/index.js
@@ -1,17 +1,21 @@
 const btnEl = document.querySelector(".btn");
 const jokeEl = document.querySelector(".joke");
+const categoryEl = document.querySelector(".category");
 
 var URL = "https://v2.jokeapi.dev/joke/Any?type=single&amount=1";
 
 btnEl.addEventListener("click",getMethod);
 
 async function getMethod(){
-    jokeEl.classList.remove("fade");
+    categoryEl.classList.remove("effect");
+    jokeEl.classList.remove("effect");
         const data = await fetch(URL).then((e)=>
             e.json());
         if(data){
             console.log(data);
+            categoryEl.innerHTML =`Category: ${data.category}`;
+            categoryEl.classList.add("effect");
             jokeEl.innerHTML = data.joke;
-            jokeEl.classList.add("fade");
+            jokeEl.classList.add("effect");
         }
     }
